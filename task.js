c832563f50920270ba41c55336f5253f98f2c927
Shawn Marcus <smarcus@post.com>
2021-10-19 12:12:54 -0400

    Added display all tasks to the user feature.

--- task.js
+++ task.js
@@ -12,12 +12,33 @@ What would you like to do? (Please enter one of the options below):
 
 `;
 
+// For displaying tasks to the user
+let showTasks = ``;
+
 // This prompt displays the menu for the user to select an option. 
 // Setting the user's response to the userInput variable lets us capture the user's input to use throughout the file.
 let userInput = prompt(menu);
 
 // Loops and continues to display as long as the user does not enter CLOSE. It will continue to prompt the user with the menu until the user enters CLOSE to end the Task Manager.
 while (userInput !== `CLOSE`){
+
+    // Checks to make sure userInput = TASKS
+    if (userInput === `TASKS`){
+    // Each item inside the tasks const variable array will be run in this For Of loop. 
+    // The For Of loop is used in this case to concatenate each item in the tasks array to the showTasks string variable.   
+    for (task of tasks){
+
+    // 
+    showTasks += `${task}\n`;
+    // Above is the same as showTasks = showTasks + `${task}\n;    
+
+    }
+    // Display the current tasks to the user.
+    alert(showTasks);
+
+    // This sets the value of the showTasks string variable back to an empty string
+    showTasks = "";
+
     // this code displays the menu again.
     userInput = prompt(menu);
 }
